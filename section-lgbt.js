3d9f7ead0e57e1853162ddfbfc921106a854d255
RomainTrchy <91048453+RomainTrchy@users.noreply.github.com>
2022-10-28 10:57:28 +0000

    Invalid DateTime

--- section-lgbt.js
+++ section-lgbt.js
@@ -1,50 +1,20 @@
-// const allCross = document.querySelectorAll(".visible-panel img");
-// console.log(allCross);
-
-// allCross.forEach((element) => {
-//   element.addEventListener("mouseenter", function () {
-//     const height = this.parentNode.parentNode.childNodes[3].scrollHeight;
-
-//     const currentChoice = this.parentNode.parentNode.childNodes[3];
-
-//     // console.log(this.src);
-//     if (this.src.includes("plus")) {
-//       this.src = "images/minus.png";
-//       gsap.to(currentChoice, {
-//         duration: 0.2,
-//         height: height + 40,
-//         opacity: 1,
-//         padding: "20px 15px",
-//       });
-//     } else if (this.src.includes("minus")) {
-//       this.src = "images/plus.png";
-//       gsap.to(currentChoice, {
-//         duration: 0.2,
-//         height: 0,
-//         opacity: 0,
-//         padding: "0px 15px",
-//       });
-//     }
-//   });
-// });
-
 const allPanels = document.querySelectorAll(".visible-panel");
 console.log(allPanels);
 
 allPanels.forEach((element) => {
   element.addEventListener("mouseenter", function () {
     const height = this.parentNode.childNodes[1].scrollHeight;
-    const currentChoice = this.parentNode.childNodes[1];
+    const currentChoice = document.querySelector(".toggle-panel");
     gsap.to(currentChoice, {
       duration: 0.2,
-      height: height + 40,  
+      height: height + 40,
       opacity: 1,
       padding: "20px 15px",
     });
   });
 
   element.addEventListener("mouseout", function () {
-    const currentChoice = this.parentNode.childNodes[1];
+    const currentChoice = document.querySelector(".toggle-panel");
     gsap.to(currentChoice, {
       duration: 0.2,
       height: 0,
