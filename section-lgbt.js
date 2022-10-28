1828ea46bf1d909c7daa62ddfb06805e5bb857c4
RomainTrchy <91048453+RomainTrchy@users.noreply.github.com>
2022-10-28 08:51:58 +0000

    Invalid DateTime

--- section-lgbt.js
+++ section-lgbt.js
@@ -1,24 +1,29 @@
-const allPanels = document.querySelectorAll(".visible-panel");
-console.log(allPanels);
+const allCross = document.querySelectorAll(".visible-pannel img");
+console.log(allCross);
 
-allPanels.forEach(element => {
+allCross.forEach((element) => {
   element.addEventListener("click", function () {
     const height = this.parentNode.parentNode.childNodes[3].scrollHeight;
 
     const currentChoice = this.parentNode.parentNode.childNodes[3];
 
-    if (this.src.includes("p")) {
-      gasp.to(currentChoice, 0.5, {
-        height: height,
+    // console.log(this.src);
+    if (this.src.includes("croix")) {
+      this.src = "images/minus.png";
+      gsap.to(currentChoice, {
+        duration: 0.2,
+        height: height + 40,
         opacity: 1,
-        ease: Power2.easeOut,
+        padding: "20px 15px",
       });
-      // } else if (this.src.includes("h2")) {
-      //   gasp.to(currentChoice, 0.5, {
-      //     height: 0,
-      //     opacity: 0,
-      //     ease: Power2.easeOut,
-      //   });
+    } else if (this.src.includes("minus")) {
+      this.src = "images/croix.png";
+      gsap.to(currentChoice, {
+        duration: 0.2,
+        height: 0,
+        opacity: 0,
+        padding: "0px 15px",
+      });
     }
   });
 });
