724944d4006225456a57c1d27b0aac80724d5b68
RomainTrchy <91048453+RomainTrchy@users.noreply.github.com>
2022-10-28 08:28:49 +0000

    Invalid DateTime

--- section-lgbt.js
+++ section-lgbt.js
@@ -1,9 +1,10 @@
-// Create a function which makes the toggle-panel div visible when click on the visible-panel div and hide it when click on the visible-panel div again
+const togglePanel = document.querySelector(".toggle-panel");
+const visiblePanel = document.querySelector(".visible-panel");
 
-const togglePanel = document.querySelector('.toggle-panel');
-const visiblePanel = document.querySelector('.visible-panel');
-
-visiblePanel.addEventListener('click', () => {
-  togglePanel.classList.toggle('visible');
+visiblePanel.addEventListener("click", () => {
+  if (togglePanel.style.display === "none") {
+    togglePanel.style.display = "block";
+  } else {
+    togglePanel.style.display = "none";
+  }
 });
-
