ec4c9cd6009c4b63bce1e6ab1e32d19fc2732417
RomainTrchy <91048453+RomainTrchy@users.noreply.github.com>
2022-10-28 08:04:48 +0000

    Invalid DateTime

--- section-lgbt.js
+++ section-lgbt.js
@@ -1,13 +1,26 @@
-const allCross = document.querySelectorAll('.visible-panel');
+const allCross = document.querySelectorAll(".visible-panel");
 console.log(allCross);
 
 allCross.forEach((element) => {
-    element.addEventListener('click', function(){
-
-        const height = this.parentNode.parentNode.childNodes[3].scrollHeight;
-
-        const currentChoice = this.parentNode.parentNode.childNodes[3];
-        gsap.to(currentChoice, {duration: 0.5, height: height + 40, opacity: 1,padding: '20px 0px 20px 0px', ease: 'power2.out'});
-        }
-    )
-})
+  element.addEventListener("click", function () {
+    const height = this.parentNode.parentNode.childNodes[3].scrollHeight;
+    const currentChoice = this.parentNode.parentNode.childNodes[3];
+    if (this.src.includes("visible")) {
+      gsap.to(currentChoice, {
+        duration: 0.5,
+        height: height + 40,
+        opacity: 1,
+        padding: "20px 15px",
+        ease: "power2.out",
+      });
+    } else if (this.src.includes("toggle")) {
+      gsap.to(currentChoice, {
+        duration: 0.5,
+        height: 0,
+        opacity: 0,
+        padding: 0,
+        ease: "power2.out",
+      });
+    }
+  });
+});
