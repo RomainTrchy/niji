213c67732aaab5dc28180ff631fc4c194f75992a
BuildTools <unconfigured@null.spigotmc.org>
2022-10-28 15:39:28 +0000

    Invalid DateTime

--- section-lgbt.js
+++ section-lgbt.js
@@ -4,25 +4,38 @@ function mousePanels() {
   const allPanels = document.querySelectorAll(".visible-panel");
 
   allPanels.forEach((element) => {
-    element.addEventListener("mouseenter", function () {
-      const height = this.parentNode.childNodes[3].scrollHeight;
-      const currentChoice = this.parentNode.childNodes[3];
-      gsap.to(currentChoice, {
-        duration: 0.2,
-        height: height + 40,
-        opacity: 1,
-        padding: "20px 15px",
-      });
-    });
+    element.addEventListener(
+      "mouseenter",
+      function () {
+        const height = this.parentNode.childNodes[3].scrollHeight;
+        const currentChoice = this.parentNode.childNodes[3];
+        gsap.to(currentChoice, {
+          duration: 0.2,
+          height: height + 40,
+          opacity: 1,
+          padding: "20px 15px",
+        });
 
-    element.addEventListener("mouseout", function () {
-      const currentChoice = this.parentNode.childNodes[3];
-      gsap.to(currentChoice, {
-        duration: 0.2,
-        height: 0,
-        opacity: 0,
-        padding: "0px 15px",
-      });
-    });
+        setTimeout(() => {
+          gsap.to(currentChoice, {
+            duration: 0.2,
+            height: 0,
+            opacity: 0,
+            padding: "0px 15px",
+          });
+        }, 5000);
+      },
+      false
+    );
+
+    // element.addEventListener("mouseout", function () {
+    //   const currentChoice = this.parentNode.childNodes[3];
+    //   gsap.to(currentChoice, {
+    //     duration: 0.2,
+    //     height: 0,
+    //     opacity: 0,
+    //     padding: "0px 15px",
+    //   });
+    // });
   });
 }
