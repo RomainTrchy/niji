d69fdf4a5e9208f43dc17cf29d134878afa2ad76
RomainTrchy <91048453+RomainTrchy@users.noreply.github.com>
2022-10-28 08:37:23 +0000

    Invalid DateTime

--- section-lgbt.js
+++
@@ -1,9 +0,0 @@
-function sectionLGBT(){
-    var section = document.getElementById('toggle');
-
-    if(section.style.opacity === '0'){
-        section.style.opacity = '1';
-    } else {
-        section.style.opacity = '0';
-    }
-}
