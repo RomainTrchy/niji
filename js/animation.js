d5178a9a9e9662ddd0c71692f2f1257fc9eaf2ac
BuildTools <unconfigured@null.spigotmc.org>
2022-11-12 20:48:58 +0100

    Add animations

---
+++ js/animation.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const targets = document.querySelectorAll(".hidden");
+targets.forEach((target) => {
+  observer.observe(target);
+});
